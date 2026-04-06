ce62578ce6a841b2d429a6807d3e93e5f532c15e
HizumeKazushi <hizume0308@gmail.com>
2026-04-06 17:23:48 +0900

    add:AA

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,25 +1,45 @@
 return {
+
   {
+
     "folke/snacks.nvim",
+
     opts = {
+
       dashboard = {
+
         sections = {
+
           { section = "header" },
+
           --{ section = "keys", gap = 1, padding = 1 },
+
           {
+
             section = "recent_files",
+
             title = "Recent Files",
+
             padding = 1,
+
             indent = 2,
+
             limit = 8,
           },
+
           {
+
             section = "projects",
+
             title = "Recent Projects",
+
             padding = 1,
+
             indent = 2,
+
             limit = 5,
           },
+
           { section = "startup" },
         },
       },
